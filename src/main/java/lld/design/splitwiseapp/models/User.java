537cbbb35cc367dca45d104fce02a66c6ed9799d
Shreyas <shre74024@gmail.com>
2024-06-14 13:33:29 +0530

    Mapping issue fixed

--- src/main/java/lld/design/splitwiseapp/models/User.java
+++ src/main/java/lld/design/splitwiseapp/models/User.java
@@ -1,8 +1,6 @@
 package lld.design.splitwiseapp.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +11,4 @@ public class User extends BaseModel{
     private String name;
     private String phoneNo;
     private String password;
-    @ManyToOne
-    @JoinColumn(name = "group_id")
-    private Group group;
 }
